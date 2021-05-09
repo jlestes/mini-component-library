c953ff8eecb07dff94d7e8f0bf43d33b7d4abd3f
Jon Estes <jlestes@gmail.com>
2021-05-09 07:28:39 -0500

    progress bar updates based on course

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -4,36 +4,41 @@ import styled from "styled-components";
 import { COLORS } from "../../constants";
 import VisuallyHidden from "../VisuallyHidden";
 
-const StyledProgressBar = styled.div`
+const Bar = styled.div`
   width: ${(props) => props.value}%;
   background-color: ${COLORS.primary};
   height: ${(props) =>
     props.size === "large" ? "16px" : props.size === "medium" ? "12px" : "8px"};
-  border-radius: ${(props) => (props.value < 98 ? "4px 0px 0px 4px" : "4px ")};
+  border-radius: "4px 0px 0px 4px";
 `;
 
-const Wrapper = styled.div`
-  border-radius: 4px;
-  width: 370px;
+const StyledProgressBar = styled.div`
+  border-radius: ${(props) => (props.size === "large" ? "8px" : "4px")};
   padding: ${(props) => (props.size === "large" ? "4px" : "0")};
   background-color: ${COLORS.transparentGray15};
   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
 `;
 
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
 const ProgressBar = ({ value, size }) => {
   return (
-    <Wrapper size={size}>
-      <StyledProgressBar
-        role="progressbar"
-        aria-valuenow={value}
-        aria-valuemin="0"
-        aria-valuemax="100"
-        value={value}
-        size={size}
-      >
-        <VisuallyHidden>{value} percent</VisuallyHidden>
-      </StyledProgressBar>
-    </Wrapper>
+    <StyledProgressBar
+      role="progressbar"
+      aria-valuenow={value}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      size={size}
+    >
+      <BarWrapper>
+        <Bar value={value} size={size}>
+          <VisuallyHidden>{value} percent</VisuallyHidden>
+        </Bar>
+      </BarWrapper>
+    </StyledProgressBar>
   );
 };
 
